ffb410f4eb2814f84fd0a6c6d6ebddab8ab47a20
KKilmetis8 <43760101+KKilmetis8@users.noreply.github.com>
2019-04-03 18:59:03 +0300

    Add files via upload

--- lorentzcode.js
+++ lorentzcode.js
@@ -20,8 +20,15 @@ context1.moveTo(250,500);
 context1.lineTo(250,0);
 context1.moveTo(500,250);
 context1.lineTo(0,250);
+context1.moveTo(0,0);
+context1.lineTo(500,500);
+context1.moveTo(0,500);
+context1.lineTo(500,0);
 context1.strokeStyle ="black";
 context1.stroke();
+context1.font= "18px Times New Roman";
+context1.fillText("t",240,15);
+context1.fillText("x",470,265);
 //vale x,x' kai t,t' stous akswnes mporw na to kanw me poly fudge tropo sthn html
 
 var canvas2= document.getElementById('mycanvas2');
@@ -33,28 +40,102 @@ context2.moveTo(250,500);
 context2.lineTo(250,0);
 context2.moveTo(500,250);
 context2.lineTo(0,250);
-context2.strokeStyle ="black";
+context2.strokeStyle = "black";
 context2.stroke();
+context2.font= "18px Times New Roman";
+context2.fillText("t'",240,15);
+context2.fillText("x'",470,265);
 
 //The velocity buttons
-var current_velocity=0.01,step=0.01,Ntoys=10000;
+var current_velocity=0.01, step = 0.01, Ntoys = 10000, color = '#000000';;
+var colorArray =[
+'#000000',//Black, 0      
+'#8BAFFF', //Light  blue, 1
+'#536EFF', //Medium blue, 2
+'#2C37FF', //Blue, 3
+'#2A377F', //Hard Blue, 4    
+'#FF886D', //Light Red, 5
+'#F2483F', //Medium Red, 6
+'#FF5B35', //Red, 7
+'#7F251A', //Hard red, 8
+'#FF9000', //Orange, 9 use this for current point
+'#BD90E1', //Violet, 10 for v=0
+];
 
 //+ button
 var buttonplus = document.getElementById("plus"); // 1. get the button
-buttonplus.addEventListener ("click", function() { // 3. Add event handler
-current_velocity+=step;
+buttonplus.addEventListener ("click", function() { // 2. Add event handler
+current_velocity=current_velocity+step;
+//u>0 colours
+	if (current_velocity<0.75){
+		color=colorArray[4];
+	}
+	if (current_velocity<0.75 && current_velocity>0.5){
+		color=colorArray[3];
+	}
+	if (current_velocity<0.5 && current_velocity>0.25){
+		color=colorArray[2];
+	}
+	if (current_velocity<0.25 && current_velocity>0){
+		color=colorArray[1];
+	}
+
+	//u<0 colours
+	if (current_velocity<-0.75){
+		color=colorArray[8];
+	}
+	if (current_velocity>-0.75 && current_velocity<-0.5){
+		color=colorArray[7];
+	}
+	if (current_velocity>-0.5 && current_velocity<-0.25){
+		color=colorArray[6];
+	}
+	if (current_velocity>-0.25 && current_velocity<0){
+		color=colorArray[5];
+	}
 lorentz.x=xlor(mouse.x,gamma(current_velocity),current_velocity,mouse.y);
 lorentz.y=tlor(mouse.x,gamma(current_velocity),current_velocity,mouse.y);
-draw2(lorentz.x,lorentz.y,0);  
+lorentz.x=lorentz.x+250;
+lorentz.y=250+lorentz.y;
+draw2(lorentz.x,lorentz.y,color);  
 });
 
 //- button
 var buttonminus = document.getElementById("minus");
 buttonminus.addEventListener ("click", function() { 
-current_velocity-=step;
+current_velocity=current_velocity-step;
+//u>0 colours
+	if (current_velocity<0.75){
+		color=colorArray[4];
+	}
+	if (current_velocity<0.75 && current_velocity>0.5){
+		color=colorArray[3];
+	}
+	if (current_velocity<0.5 && current_velocity>0.25){
+		color=colorArray[2];
+	}
+	if (current_velocity<0.25 && current_velocity>0){
+		color=colorArray[1];
+	}
+
+	//u<0 colours
+	if (current_velocity<-0.75){
+		color=colorArray[8];
+	}
+	if (current_velocity>-0.75 && current_velocity<-0.5){
+		color=colorArray[7];
+	}
+	if (current_velocity>-0.5 && current_velocity<-0.25){
+		color=colorArray[6];
+	}
+	if (current_velocity>-0.25 && current_velocity<0){
+		color=colorArray[5];
+	}
 lorentz.x=xlor(mouse.x,gamma(current_velocity),current_velocity,mouse.y);
 lorentz.y=tlor(mouse.x,gamma(current_velocity),current_velocity,mouse.y);
-draw2(lorentz.x,lorentz.y,0);  
+lorentz.x=lorentz.x+250;
+lorentz.y=250+lorentz.y;
+draw2(lorentz.x,lorentz.y,color);  
 });
 
 // Auto button
@@ -63,52 +144,88 @@ buttonauto.addEventListener ("click", function() {
 	current_velocity=-1+1/Ntoys;
 for (var i=0; i <2*Ntoys; i++) {
 	current_velocity+=1/Ntoys;
-	lorentz.x=xlor(mouse.x,gamma(current_velocity),current_velocity,mouse.y);  //add if for colours!
+
+	//u>0 colours
+	if (current_velocity<0.75){
+		color=colorArray[4];
+	}
+	if (current_velocity<0.75 && current_velocity>0.5){
+		color=colorArray[3];
+	}
+	if (current_velocity<0.5 && current_velocity>0.25){
+		color=colorArray[2];
+	}
+	if (current_velocity<0.25 && current_velocity>0){
+		color=colorArray[1];
+	}
+
+	//u<0 colours
+	if (current_velocity<-0.75){
+		color=colorArray[8];
+	}
+	if (current_velocity>-0.75 && current_velocity<-0.5){
+		color=colorArray[7];
+	}
+	if (current_velocity>-0.5 && current_velocity<-0.25){
+		color=colorArray[6];
+	}
+	if (current_velocity>-0.25 && current_velocity<0){
+		color=colorArray[5];
+	}
+
+	lorentz.x=xlor(mouse.x,gamma(current_velocity),current_velocity,mouse.y);  
 	lorentz.y=tlor(mouse.x,gamma(current_velocity),current_velocity,mouse.y);
 	lorentz.x=lorentz.x+250;
-		lorentz.y=250+lorentz.y
-	draw2(lorentz.x,lorentz.y,0);  
-}});  
+	lorentz.y=250+lorentz.y;
+	draw2(lorentz.x,lorentz.y,color);
+	}
+	current_velocity=0.01;
+});
 
-var colorArray =[
-'#000000',//Black
-'#BDD3DE', //Light  blue
-'#FFFFFF', //Medium blue
-'#BDD3DE', //Blue,
-'#FF9000', //Hard Blue,    
-'#FFFFFF', //Light Red,
-'#BDD3DE', //Medium Red,
-'#FF9000', //Red, 
-'#FFFFFF', //Hard red,
-'#FF9000', //Orange, use this for current point
-];
+//Velocity Display
+var vd=document.getElementById('velocitydisplay');
+var node= document.createTextNode("v= "+current_velocity);
+vd.appendChild(node);
 
-var color=0;
-//u>0 colours
-if (current_velocity<0.75)
-	color=colorArray[1];
-if (current_velocity<0.75 && current_velocity>0.5)
-	color=2;//add color arrays
-if (current_velocity<0.5 && current_velocity>0.25)
-	color=3;
-if (current_velocity<0.25 && current_velocity>0)
-	color=4;
-//u<0 colours
-if (current_velocity<-0.75)
-	color=5;
-if (current_velocity>-0.75 && current_velocity<-0.5)
-	color=6;
-if (current_velocity>-0.5 && current_velocity<-0.25)
-	color=7;
-if (current_velocity>-0.25 && current_velocity<0)
-	color=8;
+//Clean Button
+var buttonclean=document.getElementById("clean"); 
+buttonclean.addEventListener("click", function(){
+	
+	context1.clearRect(0,0,canvas1.width,canvas1.height); //clean
+	context2.clearRect(0,0,canvas2.width,canvas2.height);
+	//redraw canvas1
+	context1.moveTo(250,500);
+	context1.lineTo(250,0);
+	context1.moveTo(500,250);
+	context1.lineTo(0,250);
+	context1.moveTo(0,0);
+	context1.lineTo(500,500);
+	context1.moveTo(0,500);
+	context1.lineTo(500,0);
+	context1.strokeStyle ="black";
+	context1.stroke();
+	context1.fillText("t",240,15);
+	context1.fillText("x",470,265);
+	//redraw canvas2
+	context2.moveTo(250,500);
+	context2.lineTo(250,0);
+	context2.moveTo(500,250);
+	context2.lineTo(0,250);
+	context2.strokeStyle = "black";
+	context2.stroke();
+	context2.font= "18px Times New Roman";
+	context2.fillStyle='black';
+	context2.fillText("t'",240,15);
+	context2.fillText("x'",470,265);
+
+}) 
 
 var mouse = {      //Clicking position structure
 	x:undefined,
 	y:undefined
 }
 
-var lorentz ={		//Output structure. Idk if I actually need this or if it is needlessly complicated
+var lorentz ={		//Output structure. 
 	x:undefined,
 	y:undefined
 }
@@ -116,10 +233,9 @@ var lorentz ={		//Output structure. Idk if I actually need this or if it is need
 function draw1(x,y) {
 	this.x=x;
 	this.y=y;
-	this.color=color;
 	context1.beginPath();
 	context1.arc(this.x, this.y, 3, 0, Math.PI * 2, false);
-	context1.fillStyle = "black"
+	context1.fillStyle = "black";
 	context1.fill();
 	}
 
@@ -129,7 +245,7 @@ function draw2(x,y,color) {
 	this.color=color;
 	context2.beginPath();
 	context2.arc(this.x, this.y, 3, 0, Math.PI * 2, false);
-	context2.fillStyle = colorArray[this.color];
+	context2.fillStyle = this.color;
 	context2.fill();
 	}
 
@@ -141,14 +257,14 @@ window.addEventListener('click',  //Listens for clicks and
 	mouse.y= event.pageY-175;
 	console.log("x:"+mouse.x+"y:"+mouse.y);
 	draw1(mouse.x,mouse.y);
-		mouse.x=mouse.x-250;    //Makes for x 0->-250, 250->0, 500->250	
-		mouse.y=250-mouse.y;
+	mouse.x=mouse.x-250;    //Makes for x 0->-250, 250->0, 500->250	
+	mouse.y=mouse.y-250;
 	
-		lorentz.x=xlor(mouse.x,gamma(0.01),0.01,mouse.y);
-		lorentz.y=tlor(mouse.x,gamma(0.01),0.01,mouse.y);
-		// console.log("x:"+lorentz.x+"y:"+lorentz.y);
-		lorentz.x=lorentz.x+250;
-		lorentz.y=250+lorentz.y
-		draw2(lorentz.x,lorentz.y,colorArray[2]);
+	lorentz.x=xlor(mouse.x,gamma(0.01),0.01,mouse.y);
+	lorentz.y=tlor(mouse.x,gamma(0.01),0.01,mouse.y);
+	lorentz.x=lorentz.x+250;
+	lorentz.y=250+lorentz.y;
+	
+	console.log("x:"+lorentz.x+"y:"+lorentz.y);
+	draw2(lorentz.x,lorentz.y,colorArray[9]);
 	})
-
